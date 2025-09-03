6edca2e33c0c69416c4c826e64e0c9ee1e489198
booldook <booldook@gmail.com>
2025-09-03 11:25:15 +0900

    ejs bookejs

--- app03.js
+++ app03.js
@@ -1,8 +1,17 @@
 const express = require("express")
 const app = express()
+const path = require("path")
+const books = require("./mock/book-mock")
 app.listen(3000)
+/* ejs 설정 */
+app.set("view engine", "ejs")
+app.set("views", path.resolve(__dirname, "./views"))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use("/", express.static("./public"))
+app.get("/books", (req, res) => {
+  const ejs = { title: "도서리스트" }
+  res.status(200).render("book/list", ejs)
+})
---
+++ mock/book-mock.js
@@ -0,0 +1,8 @@
+const books = [
+  { id: 1, title: "별주부전", content: "용왕이 토끼의 간을..." },
+  { id: 2, title: "홍길동전", content: "아버지를 아버지라..." },
+  { id: 3, title: "춘향전", content: "변사또가 춘향이를..." },
+  { id: 4, title: "구운몽전", content: "꿈속에서 선녀가..." },
+]
+
+module.exports = books
