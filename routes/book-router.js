4df19ed9feace875afb6a9474f483e78896a87da
booldook <booldook@gmail.com>
2025-09-04 14:54:58 +0900

    book-svc booklist완성

--- routes/book-router.js
+++ routes/book-router.js
@@ -2,9 +2,13 @@ const express = require("express")
 const router = express.Router()
 const { bookList } = require("../services/mysql/book-svc")
 
-router.get("/", bookList(), async (req, res, next) => {
-  const list = req.rs
-  res.status(200).json({ success: "OK", data: { list } })
-})
+router.get(
+  "/",
+  bookList({ order: { field: "title", sort: 1 } }),
+  async (req, res, next) => {
+    const list = req.rs
+    res.status(200).json({ success: "OK", data: { list } })
+  }
+)
 
 module.exports = router
--- services/mysql/book-svc.js
+++ services/mysql/book-svc.js
@@ -5,11 +5,11 @@ const bookList = (params) => {
   return async (req, res, next) => {
     try {
       // LIMIT 0, 10 : idx[0] 부터 10개
-      const { order = {}, page = 1 } = params || {}
-      const orderBy = `${order?.field || "id"}  ${order?.sort ? "DESC" : "ASC"}`
+      const { order, page = 1 } = params || {}
+      const orderBy = `${order?.field || "id"} ${order?.sort ? "DESC" : "ASC"}`
       const pageIdx = (page - 1) * 10
-      const sql = `SELECT * FROM book ORDER BY ? LIMIT ?, 10`
-      const [rs] = await pool.execute(sql, [orderBy, pageIdx])
+      const sql = `SELECT * FROM book ORDER BY ${orderBy} LIMIT ?, 10`
+      const [rs, fields] = await pool.execute(sql, [pageIdx])
       req.rs = rs || []
       next()
     } catch (err) {
